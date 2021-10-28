508e5bdd053260ffb6b7f36be811530873397801
opilane <opilane@HP-CZC6297BBB>
2021-10-28 11:34:37 +0300

    na4ni tut

--- prof.py
+++ prof.py
@@ -1,3 +1,8 @@
-print('Hello world')'
-print("kuidas läheb")'
-print ("ss")
+print("tere lähme mangida kivi kärit paber")
+from random import*
+int(input("sisestage number"))
+if a==1("вы выбрали камень ")
+while type (a) !=input:
+    try:
+        a=float(input("нет такого варианта"))
+    except TypeError:
